7e525744def5e295901426795589e505636f66d1
Keyshawn <keyshawn@krawl.co>
2018-11-12 07:21:07 -0500

    Added combinations

--- src/index.js
+++ src/index.js
@@ -8,10 +8,18 @@ import combos from 'combos'
 
 const store = createStore(() => {
     const permutations = combos({
-        dinner: [1,2,3],
-        shopping: [1,2,3],
-        spa: [1,2,3],
-        gifts: [1,2,3]
+        dinner: [{location: ["Aldo", "Bosco", "Bahama Breeze"], value: 65, description: "All inclusive dinner"},
+                 {location:["Ciao Bella", "Buckley's", "The Beauty Shop", "Ecco"], value: 90, description: "All inclusive dinner"},
+                 {location:["Flight", "Catherine and Mary's", "Char", "Southern Social"], value: 115, description: "All inclusive dinner"}],
+        shopping: [{location: ["Black Friday!"], value: 100, description: "$150 for Black Friday shopping! "},
+                   {location: ["Black Friday!"], value: 200, description: "$250 for Black Friday shopping! "},
+                   {location: ["Black Friday!"], value: 300, description: "$350 for Black Friday shopping!"}],
+        spa: [{location: ["Relax Nail Spa"], value: 60, description: "'Basic Facial'"},
+              {location: ["Relax Nail Spa"], value: 75, description: "'Mani and Spa Pedi' with nail and toe polish and file"},
+              {location: ["The Spa Midtown"], value: 115, description: "'Herbal Fusion Massage' 60 minutes"}]
+        gifts: [{location: ["Amazon"], value: 50, description: "Hair Package: Mielle Gummy Healthy Hair Adult Vitamins, Tea tree oil, "},
+                {location: ["Amazon"], value: 60, description: "Sounds Package:  JBL Clip 3 Speaker"},
+                {locaton: ["Amazon"], value: 100, description: "Feel Warm Package: Electric Heated Throw Blanket, Ceramic Space Heater"}]
     })
     function sum(total, num) {
         return total + num;
